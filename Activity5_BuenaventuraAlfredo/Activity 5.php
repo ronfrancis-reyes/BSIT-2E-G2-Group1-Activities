fc8e5411f1b7f8fc8adc2cd70953bf1f8164b528
unknown <buenaventuraalfred15@gmail.com>
2026-03-02 19:50:04 +0800

    Revise my Activity 5 - PHP

--- Activity5_BuenaventuraAlfredo/Activity 5.php
+++ Activity5_BuenaventuraAlfredo/Activity 5.php
@@ -1,29 +1,34 @@
 <?php
-$item1_qty = 2;
-$item1_price = 100;
-$item1_total = $item1_qty * $item1_price;
+function createReceipt($items) {
+    $overall_total = 0;
+    $item_number = 1;
 
-$item2_qty = 7;
-$item2_price = 35;
-$item2_total = $item2_qty * $item2_price;
+    echo "QTY   DESC      AMT    Total\n";
+    echo "---------------------------------\n";
 
-$item3_qty = 1;
-$item3_price = 350;
-$item3_total = $item3_qty * $item3_price;
+    foreach ($items as $item) {
 
-$item4_qty = 2;
-$item4_price = 20;
-$item4_total = $item4_qty * $item4_price;
+        $qty = $item['qty'];
+        $amt = $item['amt'];
+        $total = $qty * $amt;
 
-$overall_total = $item1_total + $item2_total + $item3_total + $item4_total;
+        echo "(" . $qty . ")   ITEM " . $item_number . "    " . $amt . "    " . $total . "\n";
 
-echo "QTY     DESC        AMT    Total\n";
-echo "-------------------------------------\n";
-echo "(" . $item1_qty . ")     ITEM 1      " . $item1_price . "     " . $item1_total . "\n";
-echo "(" . $item2_qty . ")     ITEM 2      " . $item2_price . "      " . $item2_total . "\n";
-echo "(" . $item3_qty . ")     ITEM 3      " . $item3_price .       "     " . $item3_total . "\n";
-echo "(" . $item4_qty . ")     ITEM 4      " . $item4_price . "      " . $item4_total . "\n";
-echo "-------------------------------------\n";
-echo "Overall Total               Php " . $overall_total . "\n";
+        $overall_total += $total;
+        $item_number++;
+    }
+
+    echo "---------------------------------\n";
+    echo "Overall Total         Php " . $overall_total . "\n";
+}
+
+$items = [
+    ["qty" => 2, "amt" => 100],
+    ["qty" => 7, "amt" => 35],
+    ["qty" => 1, "amt" => 350],
+    ["qty" => 2, "amt" => 20]
+];
+
+createReceipt($items);
 
 ?>
